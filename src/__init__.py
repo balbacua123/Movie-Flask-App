17708c6ebb19fcd75a6ea197b92da7da7ec00b3a
balbacua123 <143883589+balbacua123@users.noreply.github.com>
2026-03-13 01:10:21 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_pymysql import MySQL
+import pymysql  # Replace flask_pymysql
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from dotenv import load_dotenv
@@ -8,25 +8,22 @@ import os
 load_dotenv()
 
 app = Flask(__name__, template_folder='templates')
-
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-app.config['MYSQL_PORT'] = 27397
-app.config['MYSQL_SSL_CA'] = os.getenv('MYSQL_SSL_CA')
+def get_db_connection():
+    return pymysql.connect(
+        host=os.getenv('MYSQL_HOST'),
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        database=os.getenv('MYSQL_DB'),
+        port=27397,
+        ssl={'ca': os.getenv('MYSQL_SSL_CA')} if os.getenv('MYSQL_SSL_CA') else None,
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
-mysql = MySQL(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = "info"
 
 from . import routes
-
-
-
-
-
