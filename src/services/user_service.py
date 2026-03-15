b98ee0c073a659c949e45eee5e3f7f6612809301
balbacua123 <143883589+balbacua123@users.noreply.github.com>
2026-03-15 19:51:45 +0800

    Update user_service.py

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,18 +1,19 @@
-from src import bcrypt
-from src import get_db_connection
+from src import mysql, bcrypt
 
 def update_user_account(user_id, username, currentPassword, current_hashed_password, newPassword):
-    if not bcrypt.check_password_hash(current_hashed_password, currentPassword):
-        return False, 'Current Password is Incorrect!'
 
+    if not bcrypt.check_password_hash(current_hashed_password, currentPassword):
+            return False, 'Current Password is Incorrect!'
+    
     hashed_password = bcrypt.generate_password_hash(newPassword).decode('utf-8')
-    conn = get_db_connection()
-    cur = conn.cursor()
+
+    cur = mysql.connection.cursor()
     cur.execute(
         "UPDATE Users SET username=%s, password=%s WHERE user_id=%s",
         (username, hashed_password, user_id)
     )
-    conn.commit()
+
+    mysql.connection.commit()
     cur.close()
-    conn.close()
+
     return True, 'Account updated successfully!'
